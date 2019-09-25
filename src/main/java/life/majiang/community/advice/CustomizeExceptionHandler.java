3d7125b77bb00a8ae2baef987431cf653528c21c
qy6695 <1070091278@qq.com>
2019-09-25 15:11:41 +0800

    2019.9.25p37

--- src/main/java/life/majiang/community/advice/CustomizeExceptionHandler.java
+++ src/main/java/life/majiang/community/advice/CustomizeExceptionHandler.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-
+//
 @ControllerAdvice
 public class CustomizeExceptionHandler {
 
